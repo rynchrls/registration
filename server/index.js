f10d06ab5b9d756b710cc8ff6e37d4a67aca595a
Ryan Alcaraz <rynchrls18@gmail.com>
2024-02-12 23:43:45 +0800

    all

--- server/index.js
+++ server/index.js
@@ -33,7 +33,12 @@ app.use(passport.initialize())
 app.use(passport.session())
 
 // for cross resources sharing
-app.use(cors());
+app.use(cors({
+  origin: '*',
+  methods: ["GET, POST, PUT, DELETE"],
+  credentials: true,
+  allowedHeaders: true
+}));
 
 // database connect
 connectDatabase();
