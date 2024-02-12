ac31bd97f8ba84a3ad4705b6539abb626d0ccb39
Ryan Alcaraz <rynchrls18@gmail.com>
2024-02-12 22:13:37 +0800

    all

--- server/index.js
+++ server/index.js
@@ -31,7 +31,7 @@ app.use(passport.session())
 
 // for cross resources sharing
 app.use(cors({
-  origin: 'http://localhost:3000',
+  origin: 'https://registration-client-sigma.vercel.app',
   methods: "GET, POST, PUT, DELETE",
   credentials: true
 }));
